d03d966e5c9b90210ff4f7882b9da2cf24e2a894
DragonMaxwell <carlosrbatista1980@gmail.com>
2025-12-28 11:57:17 -0300

    initial commit

---
+++ Launcher v2/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Launcher_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.strtGameBtn = new System.Windows.Forms.Button();
+            this.downloadLbl = new System.Windows.Forms.Label();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.minimizeBtn = new System.Windows.Forms.Button();
+            this.patchNotes = new System.Windows.Forms.WebBrowser();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.progressBar1.ForeColor = System.Drawing.Color.DarkRed;
+            this.progressBar1.Location = new System.Drawing.Point(18, 646);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1080, 35);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // strtGameBtn
+            // 
+            this.strtGameBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.strtGameBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(121)))), ((int)(((byte)(203)))));
+            this.strtGameBtn.FlatAppearance.BorderSize = 2;
+            this.strtGameBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.strtGameBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.strtGameBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(121)))), ((int)(((byte)(203)))));
+            this.strtGameBtn.Location = new System.Drawing.Point(1107, 597);
+            this.strtGameBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.strtGameBtn.Name = "strtGameBtn";
+            this.strtGameBtn.Size = new System.Drawing.Size(225, 86);
+            this.strtGameBtn.TabIndex = 1;
+            this.strtGameBtn.Text = "START GAME";
+            this.strtGameBtn.UseVisualStyleBackColor = false;
+            this.strtGameBtn.Click += new System.EventHandler(this.strtGameBtn_Click);
+            // 
+            // downloadLbl
+            // 
+            this.downloadLbl.AutoSize = true;
+            this.downloadLbl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.downloadLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(121)))), ((int)(((byte)(203)))));
+            this.downloadLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.downloadLbl.Location = new System.Drawing.Point(908, 618);
+            this.downloadLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.downloadLbl.Name = "downloadLbl";
+            this.downloadLbl.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.downloadLbl.Size = new System.Drawing.Size(0, 20);
+            this.downloadLbl.TabIndex = 2;
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.BackgroundImage = global::Launcher_v2.Properties.Resources.close1;
+            this.closeBtn.FlatAppearance.BorderSize = 0;
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.Location = new System.Drawing.Point(1299, 0);
+            this.closeBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(51, 40);
+            this.closeBtn.TabIndex = 3;
+            this.closeBtn.UseVisualStyleBackColor = false;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            this.closeBtn.MouseEnter += new System.EventHandler(this.closeBtn_MouseEnter);
+            this.closeBtn.MouseLeave += new System.EventHandler(this.closeBtn_MouseLeave);
+            // 
+            // minimizeBtn
+            // 
+            this.minimizeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.minimizeBtn.BackgroundImage = global::Launcher_v2.Properties.Resources.minimize1;
+            this.minimizeBtn.FlatAppearance.BorderSize = 0;
+            this.minimizeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeBtn.Location = new System.Drawing.Point(1248, 0);
+            this.minimizeBtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.minimizeBtn.Name = "minimizeBtn";
+            this.minimizeBtn.Size = new System.Drawing.Size(51, 40);
+            this.minimizeBtn.TabIndex = 4;
+            this.minimizeBtn.UseVisualStyleBackColor = false;
+            this.minimizeBtn.Click += new System.EventHandler(this.minimizeBtn_Click);
+            this.minimizeBtn.MouseEnter += new System.EventHandler(this.minimizeBtn_MouseEnter);
+            this.minimizeBtn.MouseLeave += new System.EventHandler(this.minimizeBtn_MouseLeave);
+            // 
+            // patchNotes
+            // 
+            this.patchNotes.Location = new System.Drawing.Point(0, 40);
+            this.patchNotes.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.patchNotes.MinimumSize = new System.Drawing.Size(30, 31);
+            this.patchNotes.Name = "patchNotes";
+            this.patchNotes.ScrollBarsEnabled = false;
+            this.patchNotes.Size = new System.Drawing.Size(1350, 662);
+            this.patchNotes.TabIndex = 5;
+            this.patchNotes.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(17, 140);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(210, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Account";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Password";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(1095, 333);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(237, 230);
+            this.panel1.TabIndex = 10;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(17, 183);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(141, 24);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Use AutoLogin";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "AutoLogin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(72)))), ((int)(((byte)(72)))), ((int)(((byte)(72)))));
+            this.ClientSize = new System.Drawing.Size(1350, 692);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.minimizeBtn);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.downloadLbl);
+            this.Controls.Add(this.strtGameBtn);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.patchNotes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button strtGameBtn;
+        private System.Windows.Forms.Label downloadLbl;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Button minimizeBtn;
+        private System.Windows.Forms.WebBrowser patchNotes;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Launcher v2/IniFile.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher_v2
+{
+    public class IniFile : IDisposable
+    {
+        private string filePath;
+        private bool disposed = false;        
+
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string UseAutoLogin { get; set; } = "true";
+        public string SframeParam { get; set; }
+        public string LauncherBackgroundUrl { get; set; }
+
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern long WritePrivateProfileString(string section, string key, string value, string filePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string section, string key, string defaultValue, StringBuilder retVal, int size, string filePath);
+
+        public IniFile(string path)
+        {
+            filePath = path;
+            EnsureFileExists();
+            LoadAllSettings();
+        }
+
+        private void LoadAllSettings()
+        {
+            Account = ReadValueFromFile("Login", "account");
+            Password = ReadValueFromFile("Login", "password");
+            UseAutoLogin = ReadValueFromFile("Login", "useAutoLogin");
+            SframeParam = ReadValueFromFile("SframeParams", "param");
+            LauncherBackgroundUrl = ReadValueFromFile("SframeParams", "LauncherBackgroundUrl");
+        }
+
+        public void SaveAllSettings()
+        {
+            WriteValueToFile("Login", "account", Account);
+            WriteValueToFile("Login", "password", Password);
+            WriteValueToFile("Login", "useAutoLogin", UseAutoLogin);            
+        }
+
+        private void EnsureFileExists()
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    string directory = Path.GetDirectoryName(filePath);
+                    if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+                    {
+                        Directory.CreateDirectory(directory);
+                    }
+
+                    WriteValueToFile("Login", "account", "");
+                    WriteValueToFile("Login", "password", "");
+                    WriteValueToFile("Login", "useAutoLogin", UseAutoLogin);
+                    WriteValueToFile("SframeParams", "param", "");
+                    WriteValueToFile("SframeParams", "LauncherBackgroundUrl", "url for your 'index.html'");
+                }
+            }
+            catch
+            {
+                //silent fail
+            }
+        }
+
+        private void WriteValueToFile(string section, string key, string value)
+        {
+            WritePrivateProfileString(section, key, value, filePath);
+        }
+
+        private string ReadValueFromFile(string section, string key)
+        {
+            StringBuilder retVal = new StringBuilder(255);
+            GetPrivateProfileString(section, key, "", retVal, 255, filePath);
+            return retVal.ToString();
+        }
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    SaveAllSettings();
+                }
+                disposed = true;
+            }
+        }
+
+        ~IniFile()
+        {
+            Dispose(false);
+        }
+    }
+}
